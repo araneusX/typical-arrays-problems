27df82d5bf2ef30306523047e59a4f3596e24056
araneusX <araneusX@gmail.com>
2020-02-25 15:30:26 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : array.reduce((min, current) => current < min ? current : min, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : array.reduce((max, current) => current > max ? current : max, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : (array.reduce((sum, current) => sum + current, 0)) / array.length;
 }
